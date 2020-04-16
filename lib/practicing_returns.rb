6b5554ee4f59b1cccdd2949292a6914527caf80b
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-16 20:59:13 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,10 +3,10 @@ require 'pry'
 def hello(coll)
   i = 0
   while i < coll.length
+    binding.pry
     yield(coll[i])
     i += 1
   end
 end
 
-binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
